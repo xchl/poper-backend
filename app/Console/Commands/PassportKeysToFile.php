a1202abf314ed572c9c8b3ffb534b22361b323d7
xuchengliang <xuchengliang@julive.com>
2024-03-10 13:33:12 +0800

    passport 公私钥初始化命令

--- app/Console/Commands/PassportKeysToFile.php
+++ app/Console/Commands/PassportKeysToFile.php
@@ -39,6 +39,8 @@ class PassportKeysToFile extends Command
      */
     public function handle()
     {
+        $tempPath = storage_path('');
+        file_put_contents($tempPath, 'Hello, World!');
         $model = PassportKey::query()->where(['system' => 'backend'])->first();
         if (Storage::disk('storage')->put('oauth-public.key', $model->public_key)) {
             $this->info( "公钥已成功写入");
